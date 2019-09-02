7b2fd477bfb18c8db7075009f3b9408d3ea3d776
qiao <qiao39gs@163.com>
2019-09-02 15:38:30 +0800

    index slider

---
+++ js/app/indexlist.1.js
@@ -0,0 +1,38 @@
+mui.init({
+	gestureConfig:{
+		doubletap: true,
+		longtap: true,
+	}
+});
+mui.plusReady(function () {
+	mui.getJSON(
+		'http://intertest.top/zaolists',
+		function(blog){
+			//console.log(JSON.stringify(blog));
+			t1 = mui('#zlist');
+			var content = "";
+			window.blog;
+			window.blog = blog;
+			for (var i = 0; i < blog.length; i++) {
+				content += '<li class="mui-table-view-cell mui-media" data-id="'+i+'">'+
+							'<a href="javascript:;">'+
+							'<img class="mui-media-object mui-pull-right" src="image/zslider/1.jpg">'+
+							'<div class="mui-media-body">'+blog[i][3]+
+							'<p class="mui-ellipsis">'+blog[i][4]+
+							'</p></div></a></li>';
+			}
+			t1[0].innerHTML = content;
+		},'json'
+	);
+	mui('#zlists').on('tap', 'li', function(){
+		mui.openWindow({
+			url:'indexcontent.html',
+			id:'indexcontent',
+			extras:{
+				blogid:blog[this.getAttribute('data-id')][0],
+				title:blog[this.getAttribute('data-id')][3],
+				content:blog[this.getAttribute('data-id')][4]
+			}
+		})
+	});
+});
--- js/app/search.js
+++ js/app/search.js
@@ -1,11 +1,58 @@
-document.getElementById("search").addEventListener("keypress",function(event) {
+var text = document.getElementById('search');
+text.addEventListener('input',function () {
+	if(text.value.length <= 10){
+		mui.post('http://intertest.top/search',{
+			msg:text.value
+		},function(tip){
+			t1 = mui('#lb');
+			var content = "";
+			window.tip;
+			window.tip = tip;
+			for (var i = 0; i < tip.length; i++) {
+				content += '<li class="mui-table-view-cell" data-id="'+i+'">'+
+								'<a>'+tip[i][1]+'</a>'+
+							'</li>';
+			}
+			t1[0].innerHTML = content;
+		},'json'
+		);
+	}
+});
+mui('#slists').on('tap', 'li', function(){
+		console.log(this.getAttribute('data-id'));
+		mui.openWindow({
+			url:'indexcontent.html',
+			id:'indexcontent',
+			extras:{
+				blogid:blog[this.getAttribute('data-id')][0]
+			}
+		})
+});
+text.addEventListener("keypress",function(event) {
     if(event.keyCode == "13") {
-        document.activeElement.blur();//收起虚拟键盘
-		var keyword = document.getElementById('search').value;
-        toSearch(keyword);//TODO 完成搜索事件
-        event.preventDefault(); // 阻止默认事件---阻止页面刷新
+        document.activeElement.blur();
+		var keyword = text.value;
+        toSearch(keyword);
+        event.preventDefault();
     }
 });
 function toSearch(word){
-	mui.toast(word);
+	mui.post('http://intertest.top/tosearch',{
+		msg:text.value
+	},function(blog){
+		var content = "";
+		for (var i = 0; i < blog.length; i++) {
+			content += '<li class="mui-table-view-cell" data-id="'+i+'">'+
+							'<a>'+blog[i][1]+'</a>'+
+						'</li>';
+		}
+		mui.openWindow({
+			url:'searchresult.html',
+			id:'searchresult',
+			extras:{
+				blogid:blog[this.getAttribute('data-id')][0]
+			}
+		})
+	},'json'
+	);
 }
